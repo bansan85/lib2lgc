60c22ef20fb6f97fe4733dfae61aa331495d9e8f
Vincent Le Garrec <legarrec.vincent@gmail.com>
2018-09-01 13:47:39 +0200

    Rename tests

--- tests/software/gdb/gdb_backtrace_factory.cc
+++ tests/software/gdb/gdb_backtrace_factory.cc
@@ -24,6 +24,7 @@
 
 int main(int /* argc */, char* /* argv */ [])  // NS
 {
+  // Check good lines
   std::unique_ptr<llgc::software::gdb::Backtrace> bt =
       llgc::software::gdb::Backtrace::Factory(
           "#4  0x000055555571cb4c in dxfRW::read (this=0x7fffffffd4f0, "
@@ -36,6 +37,27 @@ int main(int /* argc */, char* /* argv */ [])  // NS
   assert(bt->GetIndex() == 4);
   assert(bt->GetLine() == 99);
 
+  // from in bt
+  bt = llgc::software::gdb::Backtrace::Factory(
+      "#10 0x401d7877 in gtk_marshal_NONE__NONE () "
+      "from /usr/lib/libgtk-1.2.so.0");
+  assert(bt != nullptr);
+  assert(bt->HasSource());
+  assert(bt->GetFile() == "/usr/lib/libgtk-1.2.so.0");
+  assert(bt->GetName() == "gtk_marshal_NONE__NONE");
+  assert(bt->GetIndex() == 10);
+  assert(bt->GetLine() == std::numeric_limits<size_t>::max());
+
+  // No source
+  bt = llgc::software::gdb::Backtrace::Factory(
+      "#8  0x00002b2153382d4a in ?? ()");
+  assert(bt != nullptr);
+  assert(!bt->HasSource());
+  assert(bt->GetName() == "??");
+  assert(bt->GetIndex() == 8);
+  assert(bt->GetLine() == std::numeric_limits<size_t>::max());
+
+  // Check wrong lines.
   // Line to small
   bt = llgc::software::gdb::Backtrace::Factory("1");
   assert(bt == nullptr);
@@ -118,26 +140,6 @@ int main(int /* argc */, char* /* argv */ [])  // NS
       "interface_=<optimized out>, ext=<optimized out>) at "
       "libraries/libdxfrw/src/libdxfrw.cpp:aa");
   assert(bt == nullptr);
-
-  // from in bt
-  bt = llgc::software::gdb::Backtrace::Factory(
-      "#10 0x401d7877 in gtk_marshal_NONE__NONE () "
-      "from /usr/lib/libgtk-1.2.so.0");
-  assert(bt != nullptr);
-  assert(bt->HasSource());
-  assert(bt->GetFile() == "/usr/lib/libgtk-1.2.so.0");
-  assert(bt->GetName() == "gtk_marshal_NONE__NONE");
-  assert(bt->GetIndex() == 10);
-  assert(bt->GetLine() == std::numeric_limits<size_t>::max());
-
-  // No source
-  bt = llgc::software::gdb::Backtrace::Factory(
-      "#8  0x00002b2153382d4a in ?? ()");
-  assert(bt != nullptr);
-  assert(!bt->HasSource());
-  assert(bt->GetName() == "??");
-  assert(bt->GetIndex() == 8);
-  assert(bt->GetLine() == std::numeric_limits<size_t>::max());
 
   google::protobuf::ShutdownProtobufLibrary();
 
